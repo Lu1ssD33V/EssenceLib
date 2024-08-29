3a4e1147eaaaf07930b538ac0c95c2596f754ab0
lu1ssd33v <lu1ssd33v@gmail.com>
2024-08-28 18:11:21 -0600

    style(format): fix error format

--- src/weed/essencelib/BaseArgument.php
+++ src/weed/essencelib/BaseArgument.php
@@ -6,9 +6,7 @@ use pocketmine\command\CommandSender;
 
 abstract class BaseArgument {
 
-	public function __construct(
-        public string $name
-    ){}
+	public function __construct(public string $name){}
 
     abstract public function parse(CommandSender $sender, array $args): void;
 
